a6611d0aedbfe40058ac08ec768898d0af46c69b
AliNasrollahpour <ali.nasrollahpour2006@gmail.com>
2026-06-16 14:57:22 +0530

    main logic and better output

--- elecGrid/mst.py
+++ elecGrid/mst.py
@@ -2,8 +2,6 @@ import sys
 
 # input
 data=sys.stdin.read().strip().split()
-if not data:
-    return
 it=iter(data)
 n=int(next(it))
 e=int(next(it))
@@ -15,10 +13,37 @@ for _ in range(e):
     w=int(next(it))
     edges.append((w, u, v))
 
-# Logic
+# Logic (Kruskal)
+edges.sort()
+parent=list(range(n))
+rank=[0]*n
+
+def find(x):
+    while parent[x]!=x:
+        parent[x]=parent[parent[x]]
+        x=parent[x]
+    return x
+
+total=0
+mst_edges=[]
+for w, u, v in edges:
+    ru, rv=find(u), find(v)
+    if ru!=rv:
+        if rank[ru]<rank[rv]:
+            parent[ru]=rv
+        elif rank[ru]>rank[rv]:
+            parent[rv]=ru
+        else:
+            parent[rv]=ru
+            rank[ru]+=1
+        
+        total+=w
+        mst_edges.append((u, v))
+        if len(mst_edges)==n-1:
+            break
 
 # Output
-print(total)
-print(n-1)
+sys.stdout.write(total)
+sys.stdout.write(n-1)
 for u, v in mst_edges:
-    print(u, v)
+    sys.stdout.write(u, v)
